57929a94957e26a5ef7b6481004953d28b68c699
LeeYonDu <gj5051129@gmail.com>
2025-07-11 12:51:34 +0900

    localStorage 연결

--- Context/src/App.jsx
+++ Context/src/App.jsx
@@ -1,13 +1,19 @@
 import "./index.css";
 import NavBar from "./components/navBar";
 import { ItemList } from "./components/itemList";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { CartContext } from "./context/CartContext";
 import { Item } from "./components/item";
 
 function App() {
 
-  const [numberOfItemsInCart, setNumberOfItemsInCart] = useState(0);
+  const [numberOfItemsInCart, setNumberOfItemsInCart] = useState(() => {
+  const saved = localStorage.getItem("cartCount");
+  return saved ? parseInt(saved, 10): 0; });
+
+  useEffect (() => {
+    localStorage.setItem("cartCount", numberOfItemsInCart);
+  }, [numberOfItemsInCart])
 
   return (
     <CartContext.Provider
