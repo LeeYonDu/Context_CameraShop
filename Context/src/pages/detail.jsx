c66734cfd1f50c2539fb0f1c2cbe76a0c1d7157c
LeeYonDu <gj5051129@gmail.com>
2025-07-14 12:13:57 +0900

    fix/detail page

--- Context/src/pages/detail.jsx
+++ Context/src/pages/detail.jsx
@@ -1,15 +1,16 @@
-import { Navigate, useNavigate, useParams } from "react-router";
+import { useNavigate, useParams } from "react-router";
 import { data } from "../data";
 
 export default function Detail() {
   const params = useParams();
   const currentItem = data.find((item) => item.id === +params.id);
   const navigate = useNavigate();
+
   return (
-    <div className=" w-screen flex justify-center items-center">
-      <div className="w-[500px] aspect-[4/5] bg-white flex justify-center items-center flex-col gap-10 rounded-[20px] px-10 py-10 relative">
+    <div className="w-screen min-h-screen flex justify-center items-center bg-gradient-to-b from-[#000000b7] to-[#33333336]">
+      <div className="w-full max-w-[400px] aspect-[4/5] bg-white flex flex-col justify-center items-center gap-10 rounded-[20px] px-10 py-10 relative shadow-md">
         <button
-          className="absolute top-2 right-2 bg-black/0"
+          className="absolute top-2 right-2 text-red-500 text-xl border border-transparent hover:border-red-500 rounded"
           onClick={() => navigate("/")}
         >
           ❌
@@ -17,10 +18,12 @@ export default function Detail() {
         <img
           src={currentItem.image}
           alt={currentItem.name}
-          className="w-[100%] h-auto"
+          className="w-full h-auto"
         />
-        <p className="text-4xl">{currentItem.name}</p>
-        <p className="text-4xl">₩ {currentItem.price.toLocaleString()}</p>
+        <p className="text-2xl text-center font-semibold">{currentItem.name}</p>
+        <p className="text-2xl text-center text-gray-700">
+          ₩ {currentItem.price.toLocaleString()}
+        </p>
       </div>
     </div>
   );
